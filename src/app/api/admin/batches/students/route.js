4291ba42624f6e2bf7fa0f2287524be033741046
Devesh Yadav <thisisdkyadav@gmail.com>
2025-06-29 12:30:40 +0530

    refactor: Remove unused degreeDepartmentMap and departmentCodeMap files

--- src/app/api/admin/batches/students/route.js
+++ src/app/api/admin/batches/students/route.js
@@ -1,5 +1,4 @@
 import { prisma } from "@/lib/prisma"
-import { departmentCodeMap } from "@/app/utils/departmentCodeMap"
 import { NextResponse } from "next/server"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
